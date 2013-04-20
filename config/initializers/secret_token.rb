893c9369b14981077da0c505d3a73e3cb0e5cb7d
LINA <lina@lina-Aspire-4738.(none)>
2013-04-20 09:27:54 +0630

    initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linarails::Application.config.secret_token = '9df33176fadac70277ce0dfb8362156c210674a1b5fa97746456a6aca565c3ea9f78931e415283a60794fdd015c249db92985b3c769728358c6920d2f990baa4'
